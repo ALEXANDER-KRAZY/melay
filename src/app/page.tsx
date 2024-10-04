737a9e63dcf1434c233383c5c76ef15a204eac74
ALEXANDER-KRAZY <alexandermutuku855@gmail.com>
2024-10-04 05:58:53 +0300

    Add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-
+    <div className="text-red-600">
+Hi
+<Button>Click me</Button>
     </div>
   )
 }
